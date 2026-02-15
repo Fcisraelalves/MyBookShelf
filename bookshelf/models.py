b1a47d93d6abb9ac92e1d539b771a49c6b0aeaa2
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-15 17:43:37 -0300

    feat: criar os models do projeto
    Criei um model personalizado para usuário, pois precisava que tivesse o campo de email e que o login fosse feito utilizando-o.
    
    Criei os arquivos de migração e rodei o migrate.

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+
+    email = models.EmailField(max_length=255, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Author(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+
+    title = models.CharField(max_length=255, unique=True)
+    year = models.IntegerField()
+    category = models.ForeignKey(to=Category, on_delete=models.PROTECT)
+    author = models.ForeignKey(to=Author, on_delete=models.PROTECT)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
