7b3779cf349fc748c71dea970f809b7fddc10425
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-15 22:57:39 -0300

    fix: corrigindo o problema ao criar um superuser
    Basicamente, por padrão o Django utiliza o username como campo de login, nessa aplicação iremos utilizar o email, mas, para isso, é necessário informar ao django por meio da constante USERNAME_FIELD dentro da definição do model.
    
    Além disso, faz-se necessária a criação de um model de UserManager com os métodos  'create_user' e 'create_superuser'.

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,10 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The email is mandatory')
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+    
 class User(AbstractUser):
     first_name = None
     last_name = None
+    
+    objects = UserManager()
 
     email = models.EmailField(max_length=255, unique=True)
     USERNAME_FIELD = 'email'
