fb98e034ae0d05a471d067b93c9ec42bcb552e7f
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-17 22:34:03 -0300

    feat: implementar a listagem de categorias
    Nova rota:
    - **categories/**
    
    Essa rota é responsável por rendenizar uma página que exibe todas as categorias cadastradas no sistema.

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Category
 # Create your views here.
 def index(request):
     return render(request, 'bookshelf/index.html')
+
+def categories(request):
+    categories = Category.objects.all()
+    return render(request, 'bookshelf/categories.html', {'categories' : categories})
