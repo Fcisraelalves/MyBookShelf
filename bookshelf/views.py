650e440402944e4206a43d155a995aae435cfc38
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-17 22:51:13 -0300

    feat: implementar a listagem de livros por categorias
    Nova rota:
    - **categories/<int:category_id/>**
    
    Essa rota é responsável por rendenizar a página que lista todos os livros pertencentes a uma determinada categoria.

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Category
+
 # Create your views here.
 def index(request):
     return render(request, 'bookshelf/index.html')
@@ -7,3 +8,12 @@ def index(request):
 def categories(request):
     categories = Category.objects.all()
     return render(request, 'bookshelf/categories.html', {'categories' : categories})
+
+def category(request, category_id):
+    category = Category.objects.filter(id=category_id).first()
+    books = category.book_set.all()
+    context = {
+        'category' : category,
+        'books' : books
+    }
+    return render(request, 'bookshelf/category.html', context)
