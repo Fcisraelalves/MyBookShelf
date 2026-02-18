840c3f26570cd7049a112341d575f7eb51a7a994
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-17 22:16:42 -0300

    feat: personalizar a visualização dos livros no painel administrativo

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from bookshelf.models import Book, Category, Author
 
 # Register your models here.
-admin.site.register(Book)
 admin.site.register(Category)
 admin.site.register(Author)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'year', 'category']
+    list_display_links = ['title']
+    search_fields = ['title', 'category']
+    list_per_page = 5
